2b457b82c7fd7d084c1b5f8983593335b34d5a90
Rogerio Soares <rogeryu_gap@hotmail.com>
2023-03-16 14:27:34 +0000

    Votes Update and error handling completed

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getSingleArticle, patchArticle } from "../utils/api";
+import { getSingleArticle, patchArticleVotes } from "../utils/api";
 import {Image, Card, Button, Container, Badge, Spinner} from 'react-bootstrap'
 import CommentsList from "./subcomponents/CommentsList";
 
@@ -9,7 +9,17 @@ function ArticlePage () {
     const { id } = useParams();
     const [isDataLoading, setIsDataLoading] = useState(false);
     const [articleData, setArticleData] = useState({});
-    const [articlePatch, setArticlePatch] = useState({})
+    const [error, setError] = useState(false);
+
+    const handleVotesClick = (event) => {
+        event.preventDefault();
+        patchArticleVotes(id).then((articleFromApi) => {
+            setArticleData(articleFromApi)
+        })
+        .catch((error) => {
+            setError(error)
+        })
+    };
 
     useEffect(() => {
         setIsDataLoading(true);
@@ -18,11 +28,6 @@ function ArticlePage () {
             setIsDataLoading(false);
         })
     }, [])
-    useEffect(() => {
-        patchArticle(id, articlePatch.data).then((articleFromApi) => {
-            setArticleData(articleFromApi)
-        })
-      }, [articlePatch])
     return (
         <Container>
             {isDataLoading ? 
@@ -41,7 +46,8 @@ function ArticlePage () {
                     <Card.Text>
                         Votes : <Badge bg="secondary">{articleData.votes}</Badge>
                     </Card.Text>
-                    <Button variant="dark" onClick={() => {setArticlePatch({id : id, data : { inc_votes : 1}})}}>Add a vote 👍</Button>
+                    {error ? <p>Could not update the Votes</p> :
+                    <Button variant="dark" onClick={handleVotesClick}>Add a vote 👍</Button>}
                     <br></br>
                     <Card.Text>
                         By : {articleData.author}
